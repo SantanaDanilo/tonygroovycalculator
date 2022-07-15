f2f2e045394fe3dbcda49dce832bc21afe6ab17e
SantanaDanilo <109323866+SantanaDanilo@users.noreply.github.com>
2022-07-14 23:02:49 -0300

    Inserindo arquivos da Tony Groovy

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var btn = document.querySelector(".main__btn");
+
+btn.addEventListener("click", function () {
+    var produto = document.querySelector("#produto");
+    var valorProduto = produto.value;
+
+    var desconto = document.querySelector("#desconto");
+    var valorDesconto = desconto.value;
+
+    var valorPorCento = (100 - desconto.value);
+
+    alert("O valor era de R$" + ((100 * valorProduto) / valorPorCento));
+})
